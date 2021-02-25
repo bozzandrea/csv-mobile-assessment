b4544d976c0fe7047601767471b1a1127d056505
andrea bozza <andrea.bozza@zehus.it>
2021-02-26 00:37:55 +0100

    implement basic list of friend complete

--- FriendTodo/FriendTodo/ContentView.swift
+++ FriendTodo/FriendTodo/ContentView.swift
@@ -6,18 +6,42 @@
 //
 
 import SwiftUI
+import SwiftUIRefresh
+
+struct FriendView: View {
+    let friend: Friend
+    var body: some View {
+        Text("Selected friend: \(friend.name)")
+            .font(.largeTitle)
+    }
+}
 
 struct ContentView: View {
     @State var friends: [Friend] = []
+    @State private var isShowing = false
+    
     let apiRespository = FTApiRepo()
+    
     var body: some View {
-        List(friends) { friend in
-            
-            Text(friend.username)
-                .font(.headline)
-            Text(friend.name)
-                .font(.subheadline)
-            
+        NavigationView {
+            List(friends) { friend in
+                NavigationLink(destination: FriendView(friend: friend)) {
+                    Text(friend.name)
+                }
+            }
+            .navigationTitle("Welcome")
+            .toolbar {
+                Button("About") {
+                    print("About tapped!")
+                }
+            }
+        }.pullToRefresh(isShowing: $isShowing) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                apiRespository.getFriends { (friends, error) in
+                    self.friends = friends ?? []
+                    self.isShowing = false
+                }
+            }
         }
         .onAppear {
             apiRespository.getFriends { (friends, error) in
